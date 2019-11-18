3872c71b3fa03b09a23637cf1615136d0b439983
SimpllleUser <pecheritsa.vv@gmail.com>
2019-11-18 19:41:45 +0200

    install gulp

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// // import "../layout/main.html"
-import "../src/main.scss" //Берет стили только из src
-//import "bootstrap/scss/utilities"; //Подключение 
-// import "bootstrap/scss";
-import "./testMain"
-console.log("Run web pack");
-jQuery(document).ready(function($) {
-    $('body').css('background-color', 'grey');
-});
--- src/testMain.js
+++
@@ -1,2 +0,0 @@
-const x = (x, y) => x * y;
-document.getElementById("demo").innerHTML = x(5, 5);
